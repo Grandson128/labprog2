d7690ee1765a5fc9916fe99503f5fcd6988cf201
grandson <up201407729@edu.fc.up.pt>
2021-05-18 16:50:17 +0100

    Generated codes for the lusiadasClean.txt

--- parser.c
+++ parser.c
@@ -3,91 +3,69 @@
 #include <string.h>
 #include <string.h>
 #include <ctype.h>
+#include <wchar.h>
+#include <locale.h>
+
 
 void fileWordsToKeys(const char *filename){
+    setlocale(LC_CTYPE,"pt_PT.UTF-8");
+
     FILE* file = fopen(filename, "r");
+    FILE* codedFile = fopen("lusiadasCodes.txt", "a");
     int count = 1;
-    char line[1024];
+    char *line = (char *)malloc(50*sizeof(char));;
     int i;
     char lowerChar;
 
+
     while (fgets(line, 1024, file)){
-        char *tmp = strdup(line);
-        char *tok = tmp, *end = tmp;
 
-        // while (tok != NULL) {
-        //     strsep(&end, " \n");
-            //printf("%d - %s\n",count, tok);
+        if(*line == '\0' ||  count == 1){
             
-            //if(strcmp(tok, "As") == 0)
-          
+        }else{
+            i = 0;
+            while (line[i] != '\0') {       
                 
-            if(line == '\0' ||  count == 1){
-                //printf("ignored\n");
-                
-                
-
-            }else{
-                i = 0;
-                while (line[i] != '\0') {       
+                if(isdigit(line[i]) != 0){
+                }else{
                     
-                    if(isdigit(line[i]) != 0){
-                        //printf("ignored number %c", line[i]); 
-                        if(line[i+1] == '\0') 
-                            printf("\n");
-                    }else{
-                        
-                        lowerChar = tolower(line[i]);
+                    lowerChar = tolower(line[i]);
 
-                        if(lowerChar >= 97 && lowerChar <=99){
-                            printf("2");
-                        }else if(lowerChar >= 100 && lowerChar <=102){
-                            printf("3");
-                        }if(lowerChar >= 103 && lowerChar <=105){
-                            printf("4");
-                        }if(lowerChar >= 106 && lowerChar <=108){
-                            printf("5");
-                        }if(lowerChar >= 109 && lowerChar <=111){
-                            printf("6");
-                        }if(lowerChar >= 112 && lowerChar <=115){
-                            printf("7");
-                        }if(lowerChar >= 116 && lowerChar <=118){
-                            printf("8");
-                        }if(lowerChar >= 119 && lowerChar <=122){
-                            printf("9");
-                        }
-                        if(isspace(line[i]) != 0)
-                            printf(" ");
-
-                        // if(isspace(line[i]) != 0)
-                        // printf("%d  --->  %c \n", i, line[i]);
+                    if(lowerChar >= 97 && lowerChar <=99){
+                        fprintf(codedFile,"2");
+                    }else if(lowerChar >= 100 && lowerChar <=102){
+                        fprintf(codedFile,"3");
+                    }if(lowerChar >= 103 && lowerChar <=105){
+                        fprintf(codedFile,"4");
+                    }if(lowerChar >= 106 && lowerChar <=108){
+                        fprintf(codedFile,"5");
+                    }if((lowerChar >= 109 && lowerChar <=111)){
+                        fprintf(codedFile,"6");
+                    }if(lowerChar >= 112 && lowerChar <=115){
+                        fprintf(codedFile,"7");
+                    }if(lowerChar >= 116 && lowerChar <=118){
+                        fprintf(codedFile,"8");
+                    }if(lowerChar >= 119 && lowerChar <=122){
+                        fprintf(codedFile,"9");
                     }
-                   
-                   
-                   
-                    i++;
+                    if(isspace(line[i]) != 0)
+                        fprintf(codedFile," ");
+
                 }
-                printf("\n");
+                
+                i++;
             }
-
-
-            // tok = end;
-            // count++;
-        // }
-
-      
-        free(tmp);
+            fprintf(codedFile,"\n");
+        }
         count++; 
     }
 
     fclose(file);
+    fclose(codedFile);
 }
 
 
 int main(){
-
-    fileWordsToKeys("test.txt");
-  
-
+    fileWordsToKeys("lusiadasClean.txt");
     return 0;
 }
